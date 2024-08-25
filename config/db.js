d227c17f8d8d3195228bceb453ad3123d7f2bf0f
Nandhu Krishna <nandhukrishna393@gmail.com>
2024-08-25 15:24:08 +0530

    change_database

--- config/db.js
+++ config/db.js
@@ -1,18 +1,18 @@
-const mongoose = require("mongoose");
-const dotenv = require("dotenv");
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 dotenv.config();
 
-
-const db = async() => {
-
+const db = async () => {
     try {
-        await mongoose.connect(process.env.LOC_CONN, {
-           
-        })
-        console.log("Database connected successfully");
-    } catch (error) {   
-        console.error("Database connection failed"+error);
+        await mongoose.connect(process.env.MONGODB_URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log('Database connected successfully');
+    } catch (error) {
+        console.error('Database connection failed: ', error);
         process.exit(1);
-}
-}
-module.exports = db
+    }
+};
+
+module.exports = db;
