a7806f928ea2d5bc13c38c934a35a641ef2e4c98
Nandhu Krishna <nandhukrishna393@gmail.com>
2024-08-27 12:47:37 +0530

    bug fixed

--- config/passport.js
+++ config/passport.js
@@ -7,6 +7,7 @@ const jwt = require("jsonwebtoken");
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    // callbackURL: "/auth/google/callback"
     callbackURL: "https://www.nandhu.live/auth/google/callback"
 }, async (accessToken, refreshToken, profile, done) => {
     try {
