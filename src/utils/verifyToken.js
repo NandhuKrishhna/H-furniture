1bbe24d7140302cfca01173cef7ffc233483bd21
Nandhu Krishna <166598760+NandhuKrishhna@users.noreply.github.com>
2025-05-09 18:24:01 +0530

    feat: Implement verifyToken utility for JWT authentication

---
+++ src/utils/verifyToken.js
@@ -0,0 +1,10 @@
+const verifyToken = (req) => {
+    const token = req.cookies.token;
+    if (token) {
+        return jwt.verify(token, process.env.JWT_SECRET);
+    }
+    return null;
+};
+
+
+module.exports = verifyToken
