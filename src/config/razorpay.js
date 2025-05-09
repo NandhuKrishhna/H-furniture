41764a94366fb07e35f25cb8ab73b112a66fcc58
Nandhu Krishna <166598760+NandhuKrishhna@users.noreply.github.com>
2025-05-09 18:23:55 +0530

    feat: Add Razorpay configuration for payment processing

---
+++ src/config/razorpay.js
@@ -0,0 +1,7 @@
+const Razorpay = require("razorpay");
+const instance = new Razorpay({
+    key_id: "rzp_test_HIkvoqb5Aia0Ht",
+    key_secret: "B8AvHLrPrd7RfRbGbOSvcf8N",
+});
+
+module.exports = instance
