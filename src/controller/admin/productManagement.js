416e422594042bc2001f3141782c3693c3d10dcd
Nandhu Krishna <166598760+NandhuKrishhna@users.noreply.github.com>
2025-05-10 15:44:07 +0530

    refactor: Improve code organization and structure in product management

--- src/controller/admin/productManagement.js
+++ src/controller/admin/productManagement.js
@@ -2,7 +2,7 @@ const { categoryCollection } = require("../../models/categoryModel");
 const { productCollection } = require("../../models/productModels");
 const { OK, INTERNAL_SERVER_ERROR, NOT_FOUND } = require("../../utils/http");
 const fs = require('fs').promises;
-
+const { ObjectId } = require('mongoose').Types;
 const sharp = require('sharp');
 
 function convertDate(users) {
