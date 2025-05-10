89a05bf19c0b19c96b119cbae62316733bebccd3
Nandhu Krishna <166598760+NandhuKrishhna@users.noreply.github.com>
2025-05-10 13:52:13 +0530

    refactor: Streamline productController by consolidating database calls and improving cart management logic

--- src/controller/productController.js
+++ src/controller/productController.js
@@ -1,6 +1,17 @@
+const { cartCollection } = require("../models/cartModel");
+const { couponCollection } = require("../models/couponModel");
+const { productCollection } = require("../models/productModels");
+const { userCollection } = require("../models/UserModels");
+const verifyToken = require("../utils/verifyToken");
+const { ObjectId } = require("mongodb");
+
+function calculateDiscountedPrice(originalPrice, discount) {
+    return originalPrice - (originalPrice * discount / 100);
+}
 module.exports = {
     //user products
     getUserProducts: async (req, res, next) => {
+        console.log('loging the products page')
         try {
             const page = parseInt(req.query.page) || 1;
             const limit = 8;
@@ -124,7 +135,7 @@ module.exports = {
                     pipeline.push({ $sort: { _id: 1 } });
                     break;
             }
-            const totalProductsResult = await Productdb.productCollection.aggregate([
+            const totalProductsResult = await productCollection.aggregate([
                 ...pipeline,
                 { $count: "total" }
             ]).exec();
@@ -148,7 +159,7 @@ module.exports = {
                 { $limit: limit }
             );
 
-            const products = await Productdb.productCollection.aggregate(pipeline).exec();
+            const products = await productCollection.aggregate(pipeline).exec();
 
             res.status(200).render("user/user_products", {
                 user: true,
@@ -172,14 +183,14 @@ module.exports = {
     getProductDetails: async (req, res, next) => {
         try {
 
-            const product = await Productdb.productCollection
+            const product = await productCollection
                 .findById(req.params.id)
                 .lean();
             const length = product.quantity
             if (req.session.token) {
                 const user = jwt.verify(req.session.token, process.env.JWT_SECRET);
-                const userInfo = await Userdb.userCollection.findById(user._id).lean();
-                const cart = await Cartdb.cartCollection
+                const userInfo = await userCollection.findById(user._id).lean();
+                const cart = await cartCollection
                     .findOne({ userId: new ObjectId(user._id) })
                     .lean();
                 if (cart) {
@@ -225,7 +236,7 @@ module.exports = {
                 });
             }
 
-            const product = await Productdb.productCollection.findById(productId);
+            const product = await productCollection.findById(productId);
 
             if (!product.inStock) {
                 return res.status(400).json({
@@ -244,7 +255,7 @@ module.exports = {
             const discountedPrice = calculateDiscountedPrice(product.originalprice, product.discount || 0);
             const productTotalPrice = discountedPrice * quantity;
 
-            const cart = await Cartdb.cartCollection.findOne({ userId: userId });
+            const cart = await cartCollection.findOne({ userId: userId });
 
             if (cart) {
                 const productInCart = cart.products.find((p) => p.productId.toString() === productId.toString());
@@ -269,7 +280,7 @@ module.exports = {
 
                     const updatedProductTotalPrice = discountedPrice * totalQuantity;
 
-                    await Cartdb.cartCollection.updateOne(
+                    await cartCollection.updateOne(
                         { userId: userId, 'products.productId': productId },
                         {
                             $set: {
@@ -279,7 +290,7 @@ module.exports = {
                         }
                     );
                 } else {
-                    await Cartdb.cartCollection.updateOne(
+                    await cartCollection.updateOne(
                         { userId: userId },
                         {
                             $push: {
@@ -296,10 +307,10 @@ module.exports = {
                     );
                 }
 
-                const cartDetails = await Cartdb.cartCollection.findOne({ userId: userId });
+                const cartDetails = await cartCollection.findOne({ userId: userId });
                 const updatedTotalAmount = cartDetails.products.reduce((total, product) => total + product.price, 0);
 
-                await Cartdb.cartCollection.updateOne(
+                await cartCollection.updateOne(
                     { userId: userId },
                     { $set: { totalAmount: updatedTotalAmount, finalAmount: updatedTotalAmount } }
                 );
@@ -311,7 +322,7 @@ module.exports = {
                     finalAmount: updatedTotalAmount
                 });
             } else {
-                await Cartdb.cartCollection.create({
+                await cartCollection.create({
                     userId: userId,
                     products: [{
                         productId: productId,
@@ -337,13 +348,12 @@ module.exports = {
         }
     },
 
-
     getCart: async (req, res, next) => {
         try {
             const user = verifyToken(req);
             const userId = user._id;
-            const userInfo = await Userdb.userCollection.findById(userId);
-            const cart = await Cartdb.cartCollection.findOne({ userId: userId }).populate('products.productId');
+            const userInfo = await userCollection.findById(userId);
+            const cart = await cartCollection.findOne({ userId: userId }).populate('products.productId');
 
 
             const hadAddress = userInfo.address ? 'true' : 'false';
@@ -380,7 +390,7 @@ module.exports = {
             const { productId, quantity } = req.body;
 
 
-            const product = await Productdb.productCollection.findOne({ _id: productId });
+            const product = await productCollection.findOne({ _id: productId });
             if (!product) {
                 return res.status(404).json({ message: 'Product not found' });
             }
@@ -399,7 +409,7 @@ module.exports = {
             const price = calculateDiscountedPrice(product.originalprice, product.discount || 0);
 
 
-            const updateResult = await Cartdb.cartCollection.updateOne(
+            const updateResult = await cartCollection.updateOne(
                 { userId: userId, 'products.productId': productId },
                 {
                     $set: {
@@ -412,7 +422,7 @@ module.exports = {
             if (updateResult.modifiedCount === 0) {
                 return res.status(404).json({ message: 'Cart item not found' });
             }
-            const cart = await Cartdb.cartCollection.findOne({ userId: userId });
+            const cart = await cartCollection.findOne({ userId: userId });
             if (!cart) {
                 return res.status(404).json({ message: 'Cart not found' });
             }
@@ -421,7 +431,7 @@ module.exports = {
 
 
             const finalAmount = totalAmount;
-            await Cartdb.cartCollection.updateOne(
+            await cartCollection.updateOne(
                 { userId: userId },
                 {
                     $set: {
@@ -447,76 +457,84 @@ module.exports = {
     removeFromCart: async (req, res, next) => {
         try {
             const userInfo = verifyToken(req);
+            const userId = new ObjectId(userInfo._id);
             const { productId } = req.body;
 
             // Remove the product from the cart
-            const updatedCart = await Cartdb.cartCollection.updateOne(
-                { userId: new ObjectId(userInfo._id) },
+            const updatedCart = await cartCollection.updateOne(
+                { userId },
                 { $pull: { products: { productId: new ObjectId(productId) } } }
             );
 
-            if (updatedCart.modifiedCount > 0) {
-                const cart = await Cartdb.cartCollection.findOne({ userId: new ObjectId(userInfo._id) });
-
-                let totalAmount = 0;
-                let finalAmount = 0;
-                let discount = 0;
-
-                // Recalculate totalAmount if there are products left in the cart
-                if (cart && cart.products && cart.products.length > 0) {
-                    totalAmount = cart.products.reduce((sum, item) => sum + item.price * item.quantity, 0);
-
-                    // If a coupon is applied, recalculate the discount
-                    if (cart.appliedCoupon) {
-                        const coupon = await Coupondb.couponCollection.findOne({ _id: cart.appliedCoupon });
-                        if (coupon) {
-                            discount = coupon.discountType === 'fixed'
-                                ? coupon.discountValue
-                                : (totalAmount * coupon.discountValue) / 100;
-                            discount = coupon.maxDiscount ? Math.min(discount, coupon.maxDiscount) : discount;
-                            finalAmount = totalAmount - discount;
-                        }
-                    } else {
-                        finalAmount = totalAmount;
-                    }
-                } else {
-
-                    await Cartdb.cartCollection.updateOne(
-                        { userId: new ObjectId(userInfo._id) },
-                        {
-                            $unset: { appliedCoupon: "", discountValue: "" },
-                            $set: { totalAmount: 0, finalAmount: 0 }
-                        }
-                    );
-                    return res.json({
-                        success: true,
-                        updatedTotalAmount: 0,
-                        finalAmount: 0,
-                        discountValue: 0
-                    });
-                }
-
-                // Update the cart 
-                await Cartdb.cartCollection.updateOne(
-                    { userId: new ObjectId(userInfo._id) },
-                    { $set: { totalAmount: totalAmount, finalAmount: finalAmount, discountValue: discount } }
-                );
-
-                res.json({
-                    success: true,
-                    updatedTotalAmount: totalAmount,
-                    finalAmount: finalAmount,
-                    discountValue: discount
-                });
-            } else {
-                res.status(404).json({
+            if (updatedCart.modifiedCount === 0) {
+                return res.status(404).json({
                     success: false,
                     message: 'Product not found in cart'
                 });
             }
+
+            // Fetch the updated cart
+            const cart = await cartCollection.findOne({ userId });
+
+            if (!cart || !cart.products || cart.products.length === 0) {
+                await cartCollection.updateOne(
+                    { userId },
+                    {
+                        $unset: { appliedCoupon: "", discountValue: "" },
+                        $set: { totalAmount: 0, finalAmount: 0 }
+                    }
+                );
+
+                return res.json({
+                    success: true,
+                    updatedTotalAmount: 0,
+                    finalAmount: 0,
+                    discountValue: 0
+                });
+            }
+
+            // Recalculate totalAmount
+            const totalAmount = cart.products.reduce((sum, item) => sum + item.price * item.quantity, 0);
+
+            let discount = 0;
+            let finalAmount = totalAmount;
+
+            if (cart.appliedCoupon) {
+                const coupon = await couponCollection.findOne({ _id: cart.appliedCoupon });
+                if (coupon) {
+                    discount = coupon.discountType === 'fixed'
+                        ? coupon.discountValue
+                        : (totalAmount * coupon.discountValue) / 100;
+                    if (coupon.maxDiscount) {
+                        discount = Math.min(discount, coupon.maxDiscount);
+                    }
+                    finalAmount = totalAmount - discount;
+                }
+            }
+
+            // Update totals in the cart
+            await cartCollection.updateOne(
+                { userId },
+                {
+                    $set: {
+                        totalAmount,
+                        finalAmount,
+                        discountValue: discount
+                    }
+                }
+            );
+
+            res.json({
+                success: true,
+                updatedTotalAmount: totalAmount,
+                finalAmount,
+                discountValue: discount
+            });
+
         } catch (error) {
             console.error('Error removing product from cart:', error);
             next(error);
         }
-    },
+    }
+
 }
